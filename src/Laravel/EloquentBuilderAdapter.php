a6e1db331819ff68727d4a17ada6f77088a5f0a5
XanderEVG <xanderEVG@gmail.com>
2025-03-19 14:20:35 +0500

    Адаптер для symfony и laravel

--- src/Laravel/EloquentBuilderAdapter.php
+++ src/Laravel/EloquentBuilderAdapter.php
@@ -2,6 +2,7 @@
 
 namespace Xanderevg\GridFiltersLibrary\Laravel;
 
+use Xanderevg\GridFiltersLibrary\Core\Exceptions\FilterValueException;
 use Xanderevg\GridFiltersLibrary\Core\QueryBuilderInterface;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -14,13 +15,76 @@ class EloquentBuilderAdapter implements QueryBuilderInterface
         return $this->builder;
     }
 
-    public function where(string $field, string $operator, $value): self {
+    public function where(string $field, string $operator, $value): self
+    {
         $this->builder->where($field, $operator, $value);
         return $this;
     }
 
-    public function whereBetween(string $field, $from, $to): self {
+
+    public function whereNull(string $field): self
+    {
+        $this->builder->whereNull($field);
+        return $this;
+    }
+
+    public function whereNotNull(string $field): self
+    {
+        $this->builder->whereNotNull($field);
+        return $this;
+    }
+
+    public function whereFalseOrNull(string $field): self
+    {
+        $this->builder->where(function ($query, $field) {
+            /** @var Builder $query */
+            $query->where($field, '=', false)->whereNull($field);
+        });
+
+        return $this;
+    }
+
+    public function whereDate(string $field, string $operator, string $value): self
+    {
+        $this->builder->whereDate($field, $operator, $value);
+        return $this;
+    }
+
+    public function whereBetween(string $field, $from, $to): self
+    {
         $this->builder->whereBetween($field, [$from, $to]);
+        return $this;
+    }
+
+    public function whereIn(string $field, array $value): self
+    {
+        if (empty($value)) {
+            throw new FilterValueException('Values array cannot be empty for IN clause');
+        }
+
+        $this->builder->whereIn($field, $value);
+        return $this;
+    }
+
+    public function whereNotIn(string $field, array $value): self
+    {
+        if (empty($value)) {
+            throw new FilterValueException('Values array cannot be empty for NOT IN clause');
+        }
+
+        $this->builder->whereNotIn($field, $value);
+        return $this;
+    }
+
+    public function whereLike(string $field, string $value): self
+    {
+        $this->builder->whereLike($field, $value);
+        return $this;
+    }
+
+    public function whereNotLike(string $field, string $value): self
+    {
+        $this->builder->whereNotLike($field, $value);
         return $this;
     }
 }
--- src/Symfony/DoctrineQueryBuilderAdapter.php
+++ src/Symfony/DoctrineQueryBuilderAdapter.php
@@ -3,6 +3,7 @@
 namespace Xanderevg\GridFiltersLibrary\Symfony;
 
 use Doctrine\ORM\QueryBuilder;
+use Xanderevg\GridFiltersLibrary\Core\Exceptions\FilterValueException;
 use Xanderevg\GridFiltersLibrary\Core\QueryBuilderInterface;
 
 class DoctrineQueryBuilderAdapter implements QueryBuilderInterface
@@ -14,20 +15,179 @@ class DoctrineQueryBuilderAdapter implements QueryBuilderInterface
         return $this->builder;
     }
 
-    public function where(string $field, string $operator, $value): self {
+    private function getPlaceholder(string $field, string|array $operator, $value): string
+    {
+        if (is_array($value)) {
+            $value = implode(',', $value);
+        }
+
+        $value_hash = crc32($value);
+        return str_replace('.', '_', $field).'_' . $operator . '_'.$value_hash;
+    }
+
+    public function where(string $field, string $operator, $value): self
+    {
+        $placeholder = $this->getPlaceholder($field, $operator, $value);
         $this->builder
-            ->andWhere("$field $operator :value")
-            ->setParameter('value', $value)
+            ->andWhere("$field $operator :$placeholder")
+            ->setParameter($placeholder, $value)
         ;
         return $this;
     }
 
-    public function whereBetween(string $field, $from, $to): self {
+    public function whereNull(string $field): self
+    {
+        $this->builder->andWhere("$field is null");
+        return $this;
+    }
+
+    public function whereNotNull(string $field): self
+    {
+        $this->builder->andWhere("$field is not null");
+        return $this;
+    }
+
+    public function whereFalseOrNull(string $field): self
+    {
+        $this->builder->andWhere(
+            $this->builder->expr()->orX(
+                "$field = false",
+                "$field is null"
+            )
+        );
+
+        return $this;
+    }
+
+    public function whereDate(string $field, string $operator, string $value): self
+    {
+        $placeholder = $this->getPlaceholder($field, $operator, $value);
+
+        if ($operator === '=') {
+            $start = new \DateTimeImmutable($value . ' 00:00:00');
+            $end = new \DateTimeImmutable($value . ' 23:59:59');
+
+            $placeholder_start = $placeholder.'_start';
+            $placeholder_end = $placeholder.'_end';
+
+            $this->builder
+                ->andWhere("$field BETWEEN :$placeholder_start AND :$placeholder_end")
+                ->setParameter($placeholder_start, $start)
+                ->setParameter($placeholder_end, $end)
+            ;
+
+            return $this;
+        }
+
+        if ($operator === '>') {
+            $end = new \DateTimeImmutable($value . ' 23:59:59');
+            $placeholder_end = $placeholder.'_end';
+
+            $this->builder
+                ->andWhere("$field > :$placeholder_end")
+                ->setParameter($placeholder_end, $end)
+            ;
+            return $this;
+        }
+
+        if ($operator === '>=') {
+            $start = new \DateTimeImmutable($value . ' 00:00:00');
+            $placeholder_start = $placeholder.'_start';
+
+            $this->builder
+                ->andWhere("$field >= :$placeholder_start")
+                ->setParameter($placeholder_start, $start)
+            ;
+
+            return $this;
+        }
+
+        if ($operator === '<') {
+            $start = new \DateTimeImmutable($value . ' 00:00:00');
+            $placeholder_start = $placeholder.'_start';
+
+            $this->builder
+                ->andWhere("$field < :$placeholder_start")
+                ->setParameter($placeholder_start, $start)
+            ;
+
+            return $this;
+        }
+        if ($operator === '<=') {
+            $end = new \DateTimeImmutable($value . ' 23:59:59');
+            $placeholder_end = $placeholder.'_end';
+
+            $this->builder
+                ->andWhere("$field < :$placeholder_end")
+                ->setParameter($placeholder_end, $end)
+            ;
+
+            return $this;
+        }
+
+
         $this->builder
-            ->andWhere("$field BETWEEN :from AND :to")
-            ->setParameter('from', $from)
-            ->setParameter('to', $to)
+            ->andWhere("DATE($field) $operator :$placeholder")
+            ->setParameter($placeholder, $value)
         ;
+        return $this;
+    }
+
+    public function whereBetween(string $field, string $from, string $to): self {
+        $placeholder = $this->getPlaceholder($field, 'BETWEEN', $from.$to);
+        $placeholder_from = $placeholder.'_from';
+        $placeholder_to = $placeholder.'_to';
+
+        $this->builder
+            ->andWhere("$field BETWEEN :$placeholder_from AND :$placeholder_to")
+            ->setParameter($placeholder_from, $from)
+            ->setParameter($placeholder_to, $to)
+        ;
+        return $this;
+    }
+
+    public function whereIn(string $field, array $value): self
+    {
+        if (empty($value)) {
+            throw new FilterValueException('Values array cannot be empty for IN clause');
+        }
+        $placeholder = $this->getPlaceholder($field, 'in', $value);
+        $this->builder->andWhere(
+            $this->builder->expr()->in($field, ':'.$placeholder)
+        )->setParameter($placeholder, $value);
+        return $this;
+    }
+
+
+    public function whereNotIn(string $field, array $value): self
+    {
+        if (empty($value)) {
+            throw new FilterValueException('Values array cannot be empty for NOT IN clause');
+        }
+
+        $placeholder = $this->getPlaceholder($field, 'not in', $value);
+        $this->builder->andWhere(
+            $this->builder->expr()->notIn($field, ':'.$placeholder)
+        )->setParameter($placeholder, $value);
+
+        return $this;
+    }
+
+    public function whereLike(string $field, string $value): self
+    {
+        $placeholder = $this->getPlaceholder($field, 'like', $value);
+        $this->builder->andWhere(
+            $this->builder->expr()->like($field, ':'.$placeholder)
+        )->setParameter($placeholder, $value);
+        return $this;
+    }
+
+    public function whereNotLike(string $field, string $value): self
+    {
+        $placeholder = $this->getPlaceholder($field, 'not like', $value);
+        $this->builder->andWhere(
+            $this->builder->expr()->notLike($field, ':'.$placeholder)
+        )->setParameter($placeholder, $value);
         return $this;
     }
 }
